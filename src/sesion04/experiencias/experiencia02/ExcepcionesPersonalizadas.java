3586daa79b05360a0585ab57e3e3f987cccae8b3
MarielSanchezUCSM <mariel.sanchez@estudiante.ucsm.edu.pe>
2025-09-14 23:02:56 -0500

    Add files via upload

---
+++ src/sesion04/experiencias/experiencia02/ExcepcionesPersonalizadas.java
@@ -0,0 +1,16 @@
+package Experiencia02;
+
+public class ExcepcionesPersonalizadas {
+    
+    // Excepción para saldo insuficiente
+    public static class SaldoInsuficienteException extends Exception {
+        public SaldoInsuficienteException(String mensaje) {
+            super(mensaje);
+        }
+        
+        public SaldoInsuficienteException() {
+            super("Saldo insuficiente para realizar la operación");
+        }
+    }
+    
+}
